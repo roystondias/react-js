9d91d2123ac77a5e2300bd3d29a2b43655e4be32
royston <diasroyston034@gmail.com>
2022-08-20 17:27:17 +0530

    final commit display

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import styles from './Display.module.css'
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 
 function Display() {
     const [jobDetails, setjobDetails] = useState([])
@@ -12,19 +13,17 @@ function Display() {
     const dept = connection.state.department.id;
     const URL = `https://demo.jobsoid.com/api/v1/jobs/${jobId}`;
     const DEPT_URL = `https://demo.jobsoid.com/api/v1/jobs?dept=${dept}`;
-    // console.log(dept);
     useEffect(() => {
         fetch(URL).then((data) => data.json()).then((response) => setjobDetails(response));
-        fetch(DEPT_URL).then((data_1) => data_1.json()).then((response_1) => setdeptOpening(response_1));
+        fetch(DEPT_URL).then((data) => data.json()).then((response) => setdeptOpening(response));
     }, [URL, DEPT_URL])
 
-    // console.log(deptOpening);
     return (
-        <div>
-            <h3>{jobDetails.department ? jobDetails.department.title : <p></p>} Department At Teknorix Systems Goa</h3>
-            <h2>{jobDetails.title}</h2>
+        <div className={styles.container}>
+            <h4>{jobDetails.department ? jobDetails.department.title : <p></p>} Department At Teknorix Systems Goa</h4>
+            <h1>{jobDetails.title}</h1>
             <p>{jobDetails.department ? jobDetails.department.title : <p>Department</p>}</p>
-            <a href={jobDetails.applyUrl}>Apply</a>
+            <a className={styles.link} href={jobDetails.applyUrl}>Apply</a>
             <hr></hr>
             <div className={styles.flexbox}>
                 <div className={styles.flexItem1}>
@@ -32,18 +31,16 @@ function Display() {
                     <div dangerouslySetInnerHTML={{ __html: jobDetails.description }}></div>
                 </div>
                 <div className={styles.flexItem2}>
-                    <div>Other job Opening</div>
+                    <h3>OTHER JOB OPENINGS</h3>
                     <div>
-                        {deptOpening.slice(0, 5).map((Element,index) => {
-                            {/* console.log(Element);
-                            console.log(index); */}
+                        {deptOpening.slice(0, 5).map((Element, index) => {
                             if (Element.id !== jobDetails.id) {
                                 return <div>
-                                    <div>{Element.title}</div>
-                                    <p>{Element.location.city}</p>
+                                    <h4>{Element.title}</h4>
+                                    <p><AddLocationIcon></AddLocationIcon>{Element.location.city}</p>
                                 </div>
                             }
-                            if (index===0) {
+                            if (index === 0) {
                                 return <div>
                                     <div>There are no Other jobs in this department</div>
                                 </div>
